b26055c9e4ad820da2591e04e5e95a0ab99e78fc
Avazov Og'abek <avazovogabek@Avazovs-MacBook-Pro.local>
2024-02-21 12:24:06 +0500

    Update generate_file.py

--- generate_file.py
+++ generate_file.py
@@ -15,7 +15,12 @@ test = Solution()
 print(test.build_array([1, 2, 3, 4, 5]))""")
 
     print(f"File '{file_path}' has been created.")
+    
+    return file_name
 
 if __name__ == "__main__":
     problem_name = input("Enter the name of the problem: ")
-    create_python_file(problem_name)
+    file_name = create_python_file(problem_name)
+    with open('README.md', 'a') as readme:
+        readme.write(f"\n- [{problem_name}](Python/{file_name})")
+    print("Entry added to README.md.")
