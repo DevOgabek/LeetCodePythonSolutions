d981821256a1ac4828d6259d6acfabb53e503bcc
Avazov Og'abek <avazovogabek@Avazovs-MacBook-Pro.local>
2024-02-23 20:40:15 +0500

    Resolved 'Truncate Sentence' LeetCode problem with an optimized solution

--- Python/truncate_sentence.py
+++ Python/truncate_sentence.py
@@ -3,6 +3,6 @@
 class Solution(object):
     def truncateSentence(self, s, k):
         return ''.join([word + ' ' for word in s.split(' ')[:k]])[:-1]
-
+ 
 test = Solution()
 print(test.truncateSentence("Hello how are you Contestant", 4))
