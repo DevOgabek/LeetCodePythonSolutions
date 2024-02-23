5169b007d8ed8ea55772292d7b5b4a12ff0185f3
Avazov Og'abek <avazovogabek@Avazovs-MacBook-Pro.local>
2024-02-23 20:39:41 +0500

    Resolved 'Shuffle String' LeetCode problem with an optimized solution

---
+++ Python/truncate_sentence.py
@@ -0,0 +1,8 @@
+# Truncate Sentence
+
+class Solution(object):
+    def truncateSentence(self, s, k):
+        return ''.join([word + ' ' for word in s.split(' ')[:k]])[:-1]
+
+test = Solution()
+print(test.truncateSentence("Hello how are you Contestant", 4))
