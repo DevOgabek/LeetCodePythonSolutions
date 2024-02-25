0e686d05e9db39b9c19f08ef190b4c31ae490414
Avazov Og'abek <avazovogabek@Avazovs-MacBook-Pro.local>
2024-02-25 17:46:01 +0500

    Resolved 'Unique Morse Code Words' LeetCode problem with an optimized solution

---
+++ Python/maximum_product_difference_between_two_pairs.py
@@ -0,0 +1,9 @@
+# Maximum Product Difference Between Two Pairs
+
+class Solution(object):
+    def build_array(self, nums):
+        # Your solution here
+        pass
+
+test = Solution()
+print(test.build_array())
