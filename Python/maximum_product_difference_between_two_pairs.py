cf469567bf39e7d5253c9f8690a06d3c367dd43b
Og'abek <avazovogabek8@gmail.com>
2024-02-29 02:58:16 +0500

    Delete maximum_product_difference_between_two_pairs.py

--- Python/maximum_product_difference_between_two_pairs.py
+++
@@ -1,9 +0,0 @@
-# Maximum Product Difference Between Two Pairs
-
-class Solution(object):
-    def build_array(self, nums):
-        # Your solution here
-        pass
-
-test = Solution()
-print(test.build_array())
