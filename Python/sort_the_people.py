b3526c85d0e5555dba83d6d42b2f7af36c1b135c
Og'abek <avazovogabek8@gmail.com>
2024-02-28 18:53:23 +0500

    Resolved 'Sort the People' LeetCode problem with an optimized solution

--- Python/sort_the_people.py
+++ Python/sort_the_people.py
@@ -2,11 +2,13 @@
 
 class Solution(object):
     def sortPeople(self, names, heights):
-        dic = {}
-        for name, height in zip(names, heights):
-            dic[height] = name
-        sorted_heights = sorted(list(dic.keys()), reverse=True)
-        return [dic[height] for height in sorted_heights]
+        combined_data = list(zip(names, heights))
+
+        sorted_data = sorted(combined_data, key=lambda x: x[1], reverse=True)
+
+        sorted_names = [name for name, _ in sorted_data]
+
+        return sorted_names
 
 test = Solution()
 print(test.sortPeople(["Mary","John","Emma"], [180,165,170]))
