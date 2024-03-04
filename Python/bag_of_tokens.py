7eccb25b949149285a77cb103e7a22330791781f
Og'abek <avazovogabek8@gmail.com>
2024-03-04 12:35:48 +0500

    Resolved 'Bag of Tokens' LeetCode problem with an optimized solution

---
+++ Python/bag_of_tokens.py
@@ -0,0 +1,28 @@
+# Bag of Tokens
+
+class Solution(object):
+    def bagOfTokensScore(self, tokens, power):
+        tokens.sort()
+        n = len(tokens)
+        score = 0
+        max_score = 0
+        left = 0
+        right = n - 1
+        
+        while left <= right:
+            if power >= tokens[left]:
+                power -= tokens[left]
+                score += 1
+                left += 1
+                max_score = max(max_score, score)
+            elif score > 0:
+                power += tokens[right]
+                score -= 1
+                right -= 1
+            else:
+                break
+                
+        return max_score
+
+solution = Solution()
+print(solution.bagOfTokensScore([100], 50))
