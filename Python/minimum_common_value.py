2ba77f5cd2a22dde1f55607d0122d7f0b4bf4669
Og'abek <ogabek@Ogabeks-MacBook-Pro.local>
2024-03-09 07:30:59 +0500

    Resolved 'Minimum Common Value' LeetCode problem with an optimized solution

---
+++ Python/minimum_common_value.py
@@ -0,0 +1,21 @@
+# Minimum Common Value
+
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        i = 0
+        j = 0
+        common = float('inf')
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                common = nums1[i]
+                break
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        
+        return common if common != float('inf') else -1
+
+solution = Solution()
+print(solution.getCommon([1,2,3], [2,4]))
