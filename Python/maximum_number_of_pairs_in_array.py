0e004d046d6963d12feb8bbde63915d00e59928b
DevOgabek <devogabek@Ogabeks-MacBook-Pro.local>
2024-03-11 04:18:57 +0500

    Resolved 'Maximum Number of Pairs in Array' LeetCode problem with an optimized solution

--- Python/maximum_number_of_pairs_in_array.py
+++ Python/maximum_number_of_pairs_in_array.py
@@ -9,9 +9,9 @@ class Solution(object):
         formed_count = 0
         
         for count in nums_counts.values():
-            if count % 2 != 0:
-                leftover_count += 1
+            leftover_count += count % 2
             formed_count += count // 2
+            
         return [formed_count, leftover_count]
     
 solution = Solution()
