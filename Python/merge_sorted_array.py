019221a1e8b0e4e900fecb667f27f9f15f7d775e
DevOgabek <devogabek@Ogabeks-MacBook-Pro.local>
2024-03-11 09:26:45 +0500

    Resolved 'Merge Sorted Array' LeetCode problem with an optimized solution

---
+++ Python/merge_sorted_array.py
@@ -0,0 +1,24 @@
+# Merge Sorted Array
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+            
+solution = Solution()
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+solution.merge(nums1, m, nums2, n)
+print(nums1)
