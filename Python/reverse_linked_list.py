1904fcd5f95cae0f2443402845deba6ab63b975c
DevOgabek <avazovogabek8@gmail.com>
2024-03-14 12:47:57 +0500

    Resolved 'Reverse Linked List' LeetCode problem with an optimized solution

--- Python/reverse_linked_list.py
+++ Python/reverse_linked_list.py
@@ -1,9 +1,33 @@
 # Reverse Linked List
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution(object):
-    def build_array(self, nums):
-        # Your solution here
-        pass
-    
+    def reverseList(self, head):
+        new_list = None
+        current = head
+
+        while current:
+            next_node = current.next
+            current.next = new_list
+            new_list = current
+            current = next_node
+        
+        return new_list
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+
 solution = Solution()
-print(solution.build_array())
+
+reversed_list = solution.reverseList(head)
+
+while reversed_list:
+    print(reversed_list.val, end=" ")
+    reversed_list = reversed_list.next
