7231a41352bb9779784faa9a536cc0ed900cc64e
DevOgabek <avazovogabek8@gmail.com>
2024-03-14 10:43:51 +0500

    Resolved 'Is Subsequence' LeetCode problem with an optimized solution

---
+++ Python/is_subsequence.py
@@ -0,0 +1,14 @@
+# Is Subsequence
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        i = 0
+        
+        for char in t:
+            if i < len(s) and char == s[i]:
+                i += 1
+        
+        return i == len(s)
+    
+solution = Solution()
+print(solution.isSubsequence("abc", "ahbgdc"))
