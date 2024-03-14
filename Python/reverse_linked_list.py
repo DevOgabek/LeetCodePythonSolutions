e9e25f9e0aea522730b1f68ab7274a67d8af5862
DevOgabek <avazovogabek8@gmail.com>
2024-03-14 12:31:58 +0500

    Resolved 'Reverse Linked List' LeetCode problem with an optimized solution

---
+++ Python/reverse_linked_list.py
@@ -0,0 +1,9 @@
+# Reverse Linked List
+
+class Solution(object):
+    def build_array(self, nums):
+        # Your solution here
+        pass
+    
+solution = Solution()
+print(solution.build_array())
