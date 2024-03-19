aff63b5fe61e69babe96a1a5602d567ada69d37b
DevOgabek <avazovogabek8@gmail.com>
2024-03-19 20:53:52 +0500

    Resolved  LeetCode problem with an optimized solution

---
+++ Python/first_letter_to_appear_twice.py
@@ -0,0 +1,15 @@
+# First Letter to Appear Twice
+
+class Solution(object):
+    def repeatedCharacter(self, s):
+        hash_set = set()
+        
+        for char in s:
+            if char in hash_set:
+                return char
+            hash_set.add(char)
+
+        return char
+        
+solution = Solution()
+print(solution.repeatedCharacter("abccbaacz"))
