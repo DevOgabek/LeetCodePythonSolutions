6ac0a4e8623ebcebd744be221b94f0982a742c2a
DevOgabek <avazovogabek8@gmail.com>
2024-03-21 12:49:04 +0500

    Resolved 'Plus One' LeetCode problem with an optimized solution

---
+++ Python/plus_one.py
@@ -0,0 +1,9 @@
+# Plus One
+
+class Solution(object):
+    def plusOne(self, digits):
+        total = str(int(int(''.join(map(str, digits)))) + 1)
+        return [int(num) for num in total]
+                
+solution = Solution()
+print(solution.plusOne([1,2,3]))
