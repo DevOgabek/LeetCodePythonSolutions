c6baef0fa8b97e65f25118b814cdc8760edf8c25
DevOgabek <avazovogabek8@gmail.com>
2024-03-23 23:57:40 +0500

    Resolved 'Maximum Units on a Truck' LeetCode problem with an optimized solution

--- Python/maximum_units_on_a_truck.py
+++ Python/maximum_units_on_a_truck.py
@@ -2,14 +2,21 @@
 
 class Solution(object):
     def maximumUnits(self, boxTypes, truckSize):
-        boxTypes.sort(key=lambda nums: -nums[1])
-        max_truck_size = 0
+        boxTypes.sort(key=lambda x: x[1], reverse=True)
         
-        for box in boxTypes:
-            max_truck_size += min(truckSize, box[0]) * box[1]
-            truckSize -= box[0]
-            
-        return max_truck_size
+        total_units = 0
+        boxes_added = 0
+        
+        for boxes, units_per_box in boxTypes:
+            if boxes_added + boxes <= truckSize:
+                total_units += boxes * units_per_box
+                boxes_added += boxes
+            else:
+                remaining_space = truckSize - boxes_added
+                total_units += remaining_space * units_per_box
+                break
+        
+        return total_units
     
 solution = Solution()
 print(solution.maximumUnits([[1,3],[2,2],[3,1]], 4))
